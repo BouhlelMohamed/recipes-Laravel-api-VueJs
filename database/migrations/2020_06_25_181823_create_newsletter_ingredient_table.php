6c7f1c1e1e1251f5df8979f4138d21428ce02452
Bouhlel Mohamed <mohamed.bouhleel@gmail.com>
2020-06-27 10:11:37 +0200

    add template

--- database/migrations/2020_06_25_181823_create_newsletter_ingredient_table.php
+++ database/migrations/2020_06_25_181823_create_newsletter_ingredient_table.php
@@ -13,7 +13,7 @@ class CreateNewsletterIngredientTable extends Migration
      */
     public function up()
     {
-        Schema::create('newsletter_ingredient', function (Blueprint $table) {
+        Schema::create('newsletters', function (Blueprint $table) {
             $table->id();
             $table->string('email');
             $table->timestamps();
@@ -27,6 +27,6 @@ class CreateNewsletterIngredientTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('newsletter_ingredient');
+        Schema::dropIfExists('newsletters');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['middleware'=>['web']],function(){
-});
+// Route::group(['middleware'=>['web']],function(){
+// });
